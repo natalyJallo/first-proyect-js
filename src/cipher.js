8e9e177bc4c71c77d6433339d8209f794de65a49
natt17 <marketing20.nat@gmail.com>
2018-11-20 17:19:16 -0500

    cambios de js y html 2

--- src/cipher.js
+++ src/cipher.js
@@ -2,12 +2,16 @@ const encryptEffect = document.getElementById("next");
 
 const cifrar = () => {
       const textElement = document.getElementById("texto").value;
-      const offset = document.getElementById("cambios").value;
+      const offset = parseInt(document.getElementById("cambios").value);
       let cipher = "";
         for (var i = 0; i < textElement.length; i++) {
-          let ubicacion = (textElement.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
-          let palabraCifrada = String.fromCharCode(ubicacion);
-          let resultCipher = cipher += palabraCifrada;
+          const ubicacion = textElement.toUpperCase().charCodeAt(i);
+          if (ubicacion === 32) {
+            cipher += " ";
+          } else {
+            cipher = cipher + String.fromCharCode((ubicacion - 65 + offset) % 26 + 65);
+          }
+          let resultCipher = cipher;
           document.getElementById("result-message").innerHTML = resultCipher;
     }
   }
@@ -17,12 +21,17 @@ const decipherEffect = document.getElementById("next-second");
 
 const descifrar = () => {
   const textElementSecond = document.getElementById("texto-second").value;
-  const offsetSecond = document.getElementById("cambios-second").value;
+  const offsetSecond = parseInt(document.getElementById("cambios-second").value);
   let decipher = "";
     for (var j = 0; j < textElementSecond.length; j++) {
-      let ubicacionDes = (textElementSecond.toUpperCase().charCodeAt(j) + 65 - offsetSecond) % 26 + 65;
-      let palabraDescifrada = String.fromCharCode(ubicacionDes);
-      let resultDecipher = decipher += palabraDescifrada;
+      const ubicacionDes = textElementSecond.toUpperCase().charCodeAt(j);
+      const offsetSecondNew = offsetSecond%26;
+      if (ubicacionDes === 32) {
+        decipher += " ";
+      } else {
+        decipher =  decipher + String.fromCharCode((ubicacionDes + 65 - offsetSecondNew) % 26 + 65);
+      }
+      let resultDecipher = decipher;
       document.getElementById("result-second").innerHTML = resultDecipher;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 const inicio = () => {
   document.getElementById("homepage").style.display="block";
-  document.getElementById("page-cipher").style.display="block";
-  document.getElementById("answer-cipher").style.display="block";
+  document.getElementById("page-cipher").style.display="none";
+  document.getElementById("answer-cipher").style.display="none";
   document.getElementById("page-decipher").style.display="none";
   document.getElementById("answer-decipher").style.display="none";
 }
 inicio();
+
+const nextPage = document.getElementById("cifrar");
+const paginaCifrar = () => {
+  document.getElementById("homepage").style.display="none";
+  document.getElementById("page-cipher").style.display="block";
+  document.getElementById("answer-cipher").style.display="none";
+  document.getElementById("page-decipher").style.display="none";
+  document.getElementById("answer-decipher").style.display="none";
+}
+
+nextPage.addEventListener("click",paginaCifrar);
+
+ const nextPageResult = document.getElementById("next");
+ const paginaResultado = () => {
+   document.getElementById("homepage").style.display="none";
+   document.getElementById("page-cipher").style.display="none";
+   document.getElementById("answer-cipher").style.display="block";
+   document.getElementById("page-decipher").style.display="none";
+   document.getElementById("answer-decipher").style.display="none";
+ }
+ nextPageResult.addEventListener("click",paginaResultado);
+
+  const backHome = document.getElementById("next-end");
+  backHome.addEventListener("click",inicio);
+
+  const nextPageSecond = document.getElementById("descifrar");
+  const paginaDescifrarSegundo = () => {
+    document.getElementById("homepage").style.display="none";
+    document.getElementById("page-cipher").style.display="none";
+    document.getElementById("answer-cipher").style.display="none";
+    document.getElementById("page-decipher").style.display="block";
+    document.getElementById("answer-decipher").style.display="none";
+  }
+  nextPageSecond.addEventListener("click",paginaDescifrarSegundo);
+
+  const nextPageResultSecond = document.getElementById("next-second");
