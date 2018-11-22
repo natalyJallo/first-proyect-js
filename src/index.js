4272fe65b5b7fa0f88e68eff766545fd41426b92
natt17 <marketing20.nat@gmail.com>
2018-11-22 15:43:17 -0500

    subiendo cambios finales de html y js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ const paginaCifrar = () => {
 
 nextPage.addEventListener("click",paginaCifrar);
 
+const volver = document.getElementById("next-end");
+volver.addEventListener("click",inicio);
+
  const nextPageResult = document.getElementById("next");
  const paginaResultado = () => {
    document.getElementById("homepage").style.display="none";
@@ -27,9 +30,6 @@ nextPage.addEventListener("click",paginaCifrar);
    document.getElementById("answer-decipher").style.display="none";
  }
  nextPageResult.addEventListener("click",paginaResultado);
-
-  const backHome = document.getElementById("next-end");
-  backHome.addEventListener("click",inicio);
 
   const nextPageSecond = document.getElementById("descifrar");
   const paginaDescifrarSegundo = () => {
@@ -42,3 +42,21 @@ nextPage.addEventListener("click",paginaCifrar);
   nextPageSecond.addEventListener("click",paginaDescifrarSegundo);
 
   const nextPageResultSecond = document.getElementById("next-second");
+  const paginaResultadoDescifrar = ()=> {
+    document.getElementById("homepage").style.display="none";
+    document.getElementById("page-cipher").style.display="none";
+    document.getElementById("answer-cipher").style.display="none";
+    document.getElementById("page-decipher").style.display="none";
+    document.getElementById("answer-decipher").style.display="block";
+  }
+  nextPageResultSecond.addEventListener("click",paginaResultadoDescifrar);
+
+  const backHome = document.getElementById("next-end-second");
+  const volverPaginaInicio = () => {
+    document.getElementById("homepage").style.display="block";
+    document.getElementById("page-cipher").style.display="none";
+    document.getElementById("answer-cipher").style.display="none";
+    document.getElementById("page-decipher").style.display="none";
+    document.getElementById("answer-decipher").style.display="none";
+  }
+  backHome.addEventListener("click",volverPaginaInicio);
