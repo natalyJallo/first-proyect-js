cafdaf712e50c266513758dcab2215cda49dab8e
natt17 <marketing20.nat@gmail.com>
2018-12-03 18:54:03 -0500

    cambios finales de css en source

---
+++ src2/cipher.js
@@ -0,0 +1,40 @@
+window.cipher = {
+  encode : (offset,textElement) => {
+      let cipherText = "";
+      let resultEncode = "";
+        for (let i = 0; i < textElement.length; i++) {
+          let ubicacion = textElement.charCodeAt(i);
+          if (ubicacion === 32) {
+            cipherText = " ";
+          }
+          else if (ubicacion >= 65 && ubicacion <=90) {
+            cipherText = String.fromCharCode((ubicacion - 65 + offset) % 26 + 65);
+          }
+          else if (ubicacion >= 97 && ubicacion <= 122) {
+            cipherText = String.fromCharCode((ubicacion - 97 + offset) % 26 + 97);
+          }
+            resultEncode += cipherText;
+        }
+      return resultEncode;
+  },
+
+  decode : (offsetSecond,textElementSecond) => {
+      let decipherText = "";
+      let resultDecode = "";
+        for (let j = 0; j < textElementSecond.length; j++) {
+          let ubicacionDes = textElementSecond.charCodeAt(j);
+          let offsetSecondNew = offsetSecond%26;
+          if (ubicacionDes === 32) {
+            decipherText = " ";
+          }
+          else if (ubicacionDes >= 65 && ubicacionDes <=90) {
+            decipherText = String.fromCharCode(90 - (90 - ubicacionDes + offsetSecondNew) % 26);
+          }
+          else if (ubicacionDes >= 97 && ubicacionDes <= 122) {
+            decipherText = String.fromCharCode(122 - (122 - ubicacionDes + offsetSecondNew) % 26);
+          }
+          resultDecode += decipherText;
+        }
+      return resultDecode;
+    }
+};
